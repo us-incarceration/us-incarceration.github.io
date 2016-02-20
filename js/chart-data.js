94751e18fe6ba5a0334d688a8c1c0de4792ea8b5
Ariel Shapell <ajshapell@gmail.com>
2016-02-19 21:32:16 -0500

    Switched to markdown from html

---
+++ js/chart-data.js
@@ -0,0 +1,35 @@
+$(function () {
+    var data_input = eval("(" + $("#chart_data").attr("data") + ")")
+    var title_input = $("#chart_data").attr("title")
+    $('#container').highcharts({
+        chart: {
+            plotBackgroundColor: null,
+            plotBorderWidth: null,
+            plotShadow: false,
+            type: 'pie'
+        },
+        title: {
+            text: title_input
+        },
+        tooltip: {
+            pointFormat: '{series.name}: <b>{point.percentage:.1f}%</b>'
+        },
+        plotOptions: {
+            pie: {
+                allowPointSelect: true,
+                cursor: 'pointer',
+                dataLabels: {
+                    enabled: true,
+                    format: '<b>{point.name}</b>: {point.percentage:.1f} %',
+                    style: {
+                        color: (Highcharts.theme && Highcharts.theme.contrastTextColor) || 'black'
+                    }
+                }
+            }
+        },
+        series: [{
+            colorByPoint: true,
+            data: data_input
+        }]
+    });
+});
---
+++ overview.md
@@ -0,0 +1,27 @@
+---
+layout: default
+title: Current State of Correctional System in the U.S.
+---
+
+# Current State of Incarceration in the U.S.
+
+## Scale of the U.S. Correctional System
+
+[The Unites States correctional systems supervised an estimated **6.9 million** adults as of yearend 2014. 2.2 million of these persons were incarcerated in state or federal prisons or held in local jails, while 4.7 million were under community supervision (i.e. probation or parole).](/sources#CPUS) 
+
+<div id="chart_data" style="display: none;" 
+  data="[{name: 'Option 1', y: 1000000}, {name: 'Option 2', y: 2200000}]"
+  title="Title"></div>
+<div id="container" style="min-width: 310px; height: 400px; max-width: 600px; margin: 0 auto"></div>
+
+## Global Comparison
+
+## History of Mass Incarceration
+
+## Impact of Racial Bias on Incarceration
+
+## Impediments to Reform
+
+### Private Prisons
+
+### Higher Court Interpretations of Racial Bias 
