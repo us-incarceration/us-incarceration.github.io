74b1b329869ae9697722f9d4f6cac171461a807b
ajshapell <ajshapell@gmail.com>
2016-02-22 18:51:44 -0500

    Added in global incarceration chart

--- js/chart-data.js
+++ js/chart-data.js
@@ -8,8 +8,66 @@ $(function () {
               fontFamily: "'Source Sans Pro', sans-serif"
           },
       },
-      colors: ['#FF6200'],
+      colors: ['#7401DF', '#666666', '#998866', '#556655'],
       credits: { enabled: false }
+    });
+
+    var data_input = eval("(" + $("#prisoner_rate_by_country").attr("data") + ")");
+    var labels_input = eval("(" + $("#prisoner_rate_by_country").attr("labels") + ")");
+    var title_input = $("#prisoner_rate_by_country").attr("title");
+    var subtitle_input = $("prisoner_rate_by_country").attr("subtitle");
+    var credits_input = $("#prisoner_rate_by_country").attr("credits");
+    $('#prisoner_rate_by_country').highcharts({
+        chart: {
+            type: 'bar',
+            events: {
+                load: function(){
+                    var p = this.series[0].points[0];
+                    this.tooltip.refresh(p);
+                }
+            }
+        },
+        title: {
+            text: title_input
+        },
+        subtitle: {
+            text: subtitle_input
+        },
+        xAxis: {
+            categories: labels_input,
+            title: {
+                text: null
+            }
+        },
+        yAxis: {
+            min: 0,
+            title: {
+                text: 'Prison Population Rate (per 100,000 national population)',
+            },
+            labels: {
+                overflow: 'justify'
+            },
+            plotLines: [{
+                color: '#FF6200',
+                value: '90',
+                width: '2',
+                zIndex: 1000,
+                label: {
+                    text: 'Western European Avg.: 90',
+                    verticalAlign: 'bottom',
+                    style: { color: 'black' },
+                    textAlign: 'right',
+                }
+            }]
+        },
+        tooltip: {
+            pointFormat: '<b>{point.x:}</b><strong>{point.y}</strong>'
+        },
+        series: [{
+            showInLegend: false,
+            data: data_input,
+            color: '#A4A4A4',
+        }]
     });
 
     var data_input = eval("(" + $("#fed_state_prisoners_1925_2014").attr("data") + ")");
@@ -21,7 +79,7 @@ $(function () {
             events: {
                 load: function(){
                     var p = this.series[0].points[89];
-                    this.tooltip.refresh(p);  
+                    this.tooltip.refresh(p);
                 }
             }
         },
@@ -63,7 +121,7 @@ $(function () {
             }
         },
         series: [{
-            showInLegend: false,               
+            showInLegend: false,
             data: data_input
         }]
     });
