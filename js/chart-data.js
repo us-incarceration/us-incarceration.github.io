e2f5b0c47775e2c9d127317c6c067a2a29d123a2
Ariel Shapell <ajshapell@gmail.com>
2016-02-21 22:55:43 -0500

    Added historical incarceration chart

--- js/chart-data.js
+++ js/chart-data.js
@@ -1,34 +1,69 @@
 $(function () {
-    var data_input = eval("(" + $("#chart_data").attr("data") + ")")
-    var title_input = $("#chart_data").attr("title")
-    $('#container').highcharts({
+    Highcharts.setOptions({
+      lang: {
+          thousandsSep: ','
+      },
+      chart: {
+          style: {
+              fontFamily: "'Source Sans Pro', sans-serif"
+          },
+      },
+      colors: ['#FF6200'],
+      credits: { enabled: false }
+    });
+
+    var data_input = eval("(" + $("#fed_state_prisoners_1925_2014").attr("data") + ")");
+    var title_input = $("#fed_state_prisoners_1925_2014").attr("title");
+    var credits_input = $("#fed_state_prisoners_1925_2014").attr("credits");
+    $('#fed_state_prisoners_1925_2014').highcharts({
         chart: {
-            plotBackgroundColor: null,
-            plotBorderWidth: null,
-            plotShadow: false,
-            type: 'pie'
+            type: 'area',
+            events: {
+                load: function(){
+                    var p = this.series[0].points[89];
+                    this.tooltip.refresh(p);  
+                }
+            }
         },
         title: {
             text: title_input
         },
+        xAxis: {
+            allowDecimals: false,
+            labels: {
+                formatter: function () {
+                    return this.value; // clean, unformatted number for year
+                }
+            }
+        },
+        yAxis: {
+            title: { text: ''},
+            labels: {
+                formatter: function () {
+                    return this.value / 1000000 + 'MM';
+                }
+            }
+        },
         tooltip: {
-            pointFormat: '{series.name}: <b>{point.percentage:.1f}%</b>'
+            pointFormat: '<b>{point.x:}</b><strong>{point.y}</strong>'
         },
         plotOptions: {
-            pie: {
-                allowPointSelect: true,
-                cursor: 'pointer',
-                dataLabels: {
-                    enabled: true,
-                    format: '<b>{point.name}</b>: {point.percentage:.1f} %',
-                    style: {
-                        color: (Highcharts.theme && Highcharts.theme.contrastTextColor) || 'black'
+            area: {
+                pointStart: 1925,
+                marker: {
+                    enabled: false,
+                    symbol: 'circle',
+                    radius: 2,
+                    states: {
+                        hover: {
+                            enabled: true
+                        }
                     }
                 }
             }
         },
         series: [{
-            colorByPoint: true,
+            showInLegend: false,               
             data: data_input
         }]
     });
--- overview.md
+++ overview.md
@@ -7,21 +7,29 @@ title: Current State of Correctional System in the U.S.
 
 ## Scale of the U.S. Correctional System
 
-[The Unites States correctional systems supervised an estimated **6.9 million** adults as of yearend 2014. 2.2 million of these persons were incarcerated in state or federal prisons or held in local jails, while 4.7 million were under community supervision (i.e. probation or parole).](/sources#CPUS) 
-
-<div id="chart_data" style="display: none;" 
-  data="[{name: 'Option 1', y: 1000000}, {name: 'Option 2', y: 2200000}]"
-  title="Title"></div>
-<div id="container" style="min-width: 310px; height: 400px; max-width: 600px; margin: 0 auto"></div>
+[The Unites States correctional systems supervised an estimated **6.9 million** adults as of yearend 2014, representing 2.8% of the adult population. 2.2 million of these persons were incarcerated in state or federal prisons or held in local jails, while 4.7 million were under community supervision (i.e. probation or parole).](/sources#CPUS) 
 
 ## Global Comparison
 
 ## History of Mass Incarceration
 
+<div id="fed_state_prisoners_1925_2014" 
+  style="min-width: 310px; height: 400px; margin: 0 auto"
+  title="Total Prisoners in Federal & State Institutions"
+  data="[91669, 97991, 109346, 116390, 120496, 129453, 137082, 137997, 136810, 138316, 144180, 145038, 152741, 160285, 179818, 173706, 165439, 150384, 137220, 132456, 133649, 140079, 151304, 155977, 163749, 166165, 165680, 168233, 173579, 182901, 185780, 189565, 195256, 205643, 208105, 212953, 220149, 218830, 217283, 214336, 210895, 199654, 194896, 187274, 197136, 196441, 198061, 196092, 204211, 218466, 240593, 262833, 285456, 307276, 314457, 329821, 369930, 413806, 436855, 462002, 502507, 544972, 585084, 627600, 712364, 773919, 825559, 882500, 969301, 1054702, 1125874, 1181919, 1240659, 1307154, 1363686, 1394231, 1404032, 1440144, 1468601, 1497100, 1525910, 1568674, 1596835, 1608282, 1615487, 1613803, 1598968, 1570397, 1576950, 1561525]"></div>
+
+[E. Ann Carson. Prisoners in 2014. Bureau of Justice Statistics. September 2015.  http://www.bjs.gov/content/pub/pdf/p14.pdf]("/sources#P14")
+{: .chart_source}
+
+[Prisoners under the jurisdiction of state or federal correctional authorities, December 31, 1978-2014. Bureau of Justice Statistics. Generated on Feb 21, 2016 using the Corrections Statistical Analysis Tool at www.bjs.gov.]("/sources#P78_14")
+{: .chart_source}
+
 ## Impact of Racial Bias on Incarceration
 
 ## Impediments to Reform
 
-### Private Prisons
-
 ### Higher Court Interpretations of Racial Bias 
+
+### Criminal Disenfranchisement
+
+### Private Prisons
